9920eaec2f95bfa021c8000c18fe7b76f149c605
atharv-004 <164226219+atharv-004@users.noreply.github.com>
2024-04-14 00:21:17 +0530

    Update projectFile.v

--- src/projectFile.v
+++ src/projectFile.v
@@ -5,20 +5,50 @@
 
 `define default_netname none
 
-module tt_um_example (
+module tt_um_3515_sequenceDetector (
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
-    input  wire [7:0] uio_in,   // IOs: Input path
-    output wire [7:0] uio_out,  // IOs: Output path
-    output wire [7:0] uio_oe,   // IOs: Enable path (active high: 0=input, 1=output)
-    input  wire       ena,      // will go high when the design is enabled
-    input  wire       clk,      // clock
-    input  wire       rst_n     // reset_n - low to reset
 );
 
-  // All output pins must be assigned. If not used, assign to 0.
-  assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
-  assign uio_out = 0;
-  assign uio_oe  = 0;
+    wire x = ui_in[0];
+    wire clk = ui_in[1];
+    wire reset = ui_in[2];
+    reg [7:0] seg
+
+    reg [1:0] PS;
+    reg [1:0] NS;
+    reg z;
+
+    assign uo_out = seg [7:0]
+
+  parameter S0=0, S1=1, S2=2, S3=3;
+  reg [1:0] PS, NS;
+  reg z;
+
+  always @(posedge clk or posedge reset) begin
+      if (!reset) begin
+      PS <= S0;
+      z <= 0;
+    end else begin
+      PS <= NS;
+      z <= (PS == S3);
+    end
+  end
+
+  always @(*) begin
+    case (PS)
+      S0: NS = x ? S0 : S1;
+      S1: NS = x ? S2 : S1;
+      S2: NS = x ? S3 : S1;
+      S3: NS = x ? S0 : S1;
+    endcase
+  end
+
+  always @(*) begin
+    case (z)
+      0: seg = 7'b00000010; // Display - on 7-segment (sequence not detected)
+      1: seg = 7'b11111111; // Display 8. on 7-segment (sequence detected)
+    endcase
+  end
 
 endmodule
