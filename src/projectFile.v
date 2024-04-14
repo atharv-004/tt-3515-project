af51e57495b035c2429e6ada435c6d3a595bfb7a
atharv-004 <164226219+atharv-004@users.noreply.github.com>
2024-04-14 12:25:45 +0530

    Update projectFile.v

--- src/projectFile.v
+++ src/projectFile.v
@@ -20,26 +20,23 @@
 module tt_um_3515_sequenceDetector (
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
+    input  wire [7:0] uio_in,   // IOs: Input path
     output wire [7:0] uio_out,  // IOs: Output path
     output wire [7:0] uio_oe,   // IOs: Enable path (active high: 0=input, 1=output)
     input  wire       ena,      // will go high when the design is enabled
     input  wire       clk,      // clock
     input  wire       rst_n     // reset_n - low to reset
 );
-    wire x = ui_in[0];
-    wire clk = ui_in[1];
-    wire reset = ui_in[2];
-    
     reg [7:0] seg;
     reg [1:0] PS, NS;
     reg z;
 
-    assign uo_out [7:0] = seg;
+    assign uo_out = seg;
     assign uio_out = 8'b0;
     assign uio_oe = 8'b0;
 
-    always @(posedge clk or posedge reset) begin
-        if (!reset) begin
+    always @(posedge clk or posedge rst_n) begin
+        if (!rst_n) begin
             PS <= 2'b00; // S0
             z <= 1'b0;
         end else begin
@@ -50,10 +47,10 @@ module tt_um_3515_sequenceDetector (
 
     always @(*) begin
         case (PS)
-            2'b00: NS = x ? 2'b00 : 2'b01; // S0
-            2'b01: NS = x ? 2'b10 : 2'b01; // S1
-            2'b10: NS = x ? 2'b11 : 2'b01; // S2
-            2'b11: NS = x ? 2'b00 : 2'b01; // S3
+            2'b00: NS <= ui_in[0] ? 2'b00 : 2'b01; // S0
+            2'b01: NS <= ui_in[0] ? 2'b10 : 2'b01; // S1
+            2'b10: NS <= ui_in[0] ? 2'b11 : 2'b01; // S2
+            2'b11: NS <= ui_in[0] ? 2'b00 : 2'b01; // S3
         endcase
     end
 
