496c78d0d35419c49c32d6be593eee8941084222
atharv-004 <164226219+atharv-004@users.noreply.github.com>
2024-04-18 20:51:56 +0530

    Update tb.v

--- test/tb.v
+++ test/tb.v
@@ -23,7 +23,7 @@ module tb ();
   wire [7:0] uio_oe;
 
   // Replace tt_um_example with your module name:
-  tt_um_3515_sequenceDetector tt_um_3515_sequenceDetector(
+  tt_um_3515_sequenceDetector user_project (
 
       // Include power ports for the Gate Level test:
 `ifdef GL_TEST
